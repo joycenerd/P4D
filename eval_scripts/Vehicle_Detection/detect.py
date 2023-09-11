e5cf26a335fbd3119029a4abba17c636fbaae098
joycenerd <joycenerd.cs09@nycu.edu.tw>
2023-09-11 14:26:12 +0800

    raise the confidence threshold to avoid false positive

--- eval_scripts/Vehicle_Detection/detect.py
+++ eval_scripts/Vehicle_Detection/detect.py
@@ -31,7 +31,7 @@ from utils.torch_utils import select_device, load_classifier, time_sync
 def run(weights='yolov5s.pt',  # model.pt path(s)
         source='data/images',  # file/dir/URL/glob, 0 for webcam
         imgsz=640,  # inference size (pixels)
-        conf_thres=0.25,  # confidence threshold
+        conf_thres=0.7,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
         max_det=1000,  # maximum detections per image
         device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
